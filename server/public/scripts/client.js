fdc55682b5c9bc01904faacc93c0dc5581213659
Freddy Reza <rezafreddy@gmail.com>
2022-03-11 17:48:15 -0600

    have functioning operators, history of inputs, and answer displaying.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,10 +3,18 @@ $(ready)
 
 let operator;
 
+let solution = 0;
+let userInput = '';
+
 function ready() {
     console.log('JS Works!');
     $('#equals').on('click', equalsClicked);
     $('#addition').on('click', additionClicked);
+    $('#subtract').on('click', subtractionClicked);
+    $('#multiply').on('click', multiplicationClicked);
+    $('#divide').on('click', divisionClicked);
+
+    getHistory();
 }
 
 function equalsClicked() {
@@ -18,16 +26,59 @@ function equalsClicked() {
         method: 'POST',
         data: {
         firstInput: firstInput,
-        secondInput: secondInput,
-        operator: operator
+        operator: operator,
+        secondInput: secondInput
         }
     }).then(function (response) {
         console.log(response);
-
     })
+
+    getHistory();
 }
 
 function additionClicked() {
     operator = $(this).text();
     return operator;
 }
+
+function subtractionClicked() {
+    operator = $(this).text();
+    return operator;
+}
+
+function multiplicationClicked() {
+    operator = $(this).text();
+    return operator;
+}
+
+function divisionClicked() {
+    operator = $(this).text();
+    return operator;
+}
+
+function getHistory() {
+    $.ajax({
+        url: '/inputs',
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response);
+
+        answer = response.outputs[response.outputs.length - 1];
+        userInput = renderUserInputs(response);
+        console.log(answer);
+        $('#solution').text(answer);
+        $('#userHistory').append(`<li>${userInput}=${answer}</li>`)
+
+    })
+}
+
+function renderUserInputs(theInputs) {
+    let firstUserInput = theInputs.inputs[theInputs.inputs.length - 1].firstInput;
+    let operatorUserInput = theInputs.inputs[theInputs.inputs.length - 1].operator;
+    let secondUserInput = theInputs.inputs[theInputs.inputs.length - 1].secondInput;
+
+    let finalInput = firstUserInput + operatorUserInput + secondUserInput;
+
+    return finalInput;
+}
+
--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-let inputs = [];
+let history = {
+    inputs: [],
+    outputs: []
+}
+// let inputs = [];
+// let outputs = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -12,19 +17,42 @@ app.use(express.static('server/public'));
 app.post(`/inputs`, (req, res) => {
     console.log(`POST inputs`, req.body);
     
-    inputs.push(req.body);
+    let solution = calculate(req.body);
+    history.outputs.push(solution)
+    history.inputs.push(req.body);
 
     res.sendStatus(201);
 })
 
+function calculate(inputsToCalculate) {
+    let firstInput = Number(inputsToCalculate.firstInput);
+    let secondInput = Number(inputsToCalculate.secondInput);
+    let operator = inputsToCalculate.operator;
+    let answer = 0;
+
+    switch (operator) {
+        case '+':
+            answer = firstInput + secondInput;
+            break;
+        case '-':
+            answer = firstInput - secondInput;
+            break;
+        case 'x':
+            answer = firstInput * secondInput;
+            break;
+        case '/':
+            answer = firstInput / secondInput;
+            break;
+        default:
+            break;
+    }
+    return answer;
+}
+
 app.get('/inputs', (req, res) => {
     console.log('GET inputs');
-    res.send(inputs);
+    res.send(history);
 })
-
-
-
-
 
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
